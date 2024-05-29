9c603d7fea2b4e8da76a37adf0f2710e31d1a42f
viditsuri-1 <156009654+viditsuri-1@users.noreply.github.com>
2024-05-28 20:26:59 -0400

    Update layered_model.py

--- layered_model.py
+++ layered_model.py
@@ -9,7 +9,8 @@ def define_dense_model_single_layer(input_length, activation_f='sigmoid', output
     activation_f: the activation function
     output_length: the number of outputs (number of neurons)"""
     model = keras.Sequential([
-        layers.Dense(output_length, activation=activation_f, input_shape=(input_length,))
+        keras.Input(shape=(input_length,)),
+        layers.Dense(output_length, activation=activation_f)
     ])
     return model
 
@@ -22,10 +23,10 @@ def define_dense_model_with_hidden_layer(input_length,
     activation_func_array: the activation function for the hidden layer and the output layer
     hidden_layer_size: the number of neurons in the hidden layer
     output_length: the number of outputs (number of neurons in the output layer)"""
-
     model = keras.Sequential([
-    layers.Dense(hidden_layer_size, activation=activation_func_array[0], input_shape=(input_length,)),
-    layers.Dense(output_length, activation=activation_func_array[1])
+        keras.Input(shape=(input_length,)),
+        layers.Dense(hidden_layer_size, activation=activation_func_array[0]),
+        layers.Dense(output_length, activation=activation_func_array[1])
     ])
     return model
 
@@ -45,7 +46,6 @@ def fit_mnist_model_single_digit(x_train, y_train, target_digit, model, epochs=1
     """Fit the model to the data.
     compile the model and add parameters for  the "optimizer", the loss function , 
     and the metrics, Hint: use binary crossentropy for the loss function .
-
     then fit the model on the training data. (pass the epochs and batch_size params)
     """
     y_train = binarize_labels(y_train, target_digit)
