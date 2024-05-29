450f071e2b3bfb4aacff14ef7e5c41901edf3b5f
viditsuri-1 <156009654+viditsuri-1@users.noreply.github.com>
2024-05-28 20:32:34 -0400

    Update layered_model_test.py

--- layered_model_test.py
+++ layered_model_test.py
@@ -9,21 +9,19 @@ from layered_model import binarize_labels, get_mnist_data
 
 def test_define_dense_model_single_layer():
     model = define_dense_model_single_layer(43, activation_f='sigmoid', output_length=1)
-    assert len(model.layers) == 1, "model should have 1 layer"
+    assert len(model.layers) == 2, "model should have 2 layers (Input and Dense)"
     assert model.input_shape == (None, 43), "input_shape is not correct"
-    assert model.layers[0].output_shape == (None, 1), "output_shape is not correct"
-
+    assert model.layers[1].units == 1, "output_shape is not correct"
 
 def test_define_dense_model_with_hidden_layer():
     model = define_dense_model_with_hidden_layer(43, activation_func_array=['relu', 'sigmoid'], hidden_layer_size=11, output_length=1)
-    assert len(model.layers) == 2, "model should have 2 layers"
+    assert len(model.layers) == 3, "model should have 3 layers (Input, Dense hidden, Dense output)"
     assert model.input_shape == (None, 43), "input_shape is not correct"
-    assert model.layers[0].output_shape == (None, 11), "output_shape is not correct"
-    assert model.layers[1].output_shape == (None, 1), "output_shape is not correct"
-
+    assert model.layers[1].units == 11, "hidden layer output_shape is not correct"
+    assert model.layers[2].units == 1, "output layer output_shape is not correct"
 
 def test_fit_and_predict_mnist_single_digit_one_neuron():
-    model = define_dense_model_single_layer(28 * 28, activation_f='sigmoid', output_length=1)
+    model = define_dense_model_single_layer(28*28, activation_f='sigmoid', output_length=1)
     (x_train, y_train), (x_test, y_test) = get_mnist_data()
     model = fit_mnist_model_single_digit(x_train, y_train, 2, model, epochs=5, batch_size=128)
     loss, accuracy = evaluate_mnist_model_single_digit(x_test, y_test, 2, model)
